44c4ce6906b6e80f8ce0cfa9aa81be18fdd2bfb3
Rajat Beladiya <rajatbeladiya7@gmail.com>
2021-01-09 21:42:20 +0530

    Smart contracts added

---
+++ contracts/mocks/Bat.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.3;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract Bat is ERC20 {
+    constructor() ERC20("Brave browser token", "BAT") public {}
+    
+    function faucet(address to, uint amount) external {
+        _mint(to, amount);
+    }
+}
